d1c90803f669af5742a9454cd6c78f1928b86d94
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-06-10 19:04:00 -0400

    Added some JS but not much. Added to top portion.

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const btnC = document.querySelector("#clear");
-btnC.addEventListener('click', function(){
-  console.log();
-});
+btnC.addEventListener('click', event);
 
-// console.log(btnC);
-// const btn1 = document.querySelector(#seven);
-// const btn2 = document.querySelector(#seven)
-// const btn3 = document.querySelector(#seven)
+const btn7 = document.querySelector('#seven');
+btn7.addEventListener('click', event);
+
+const btn8 = document.querySelector('#eight');
+btn8.addEventListener('click', event);
+
+const btn9 = document.querySelector('#nine');
+btn9.addEventListener('click', event);
+
 // const btn4 = document.querySelector(#seven)
 // const btn5 = document.querySelector(#seven)
 // const btn6 = document.querySelector(#seven)
@@ -20,17 +23,17 @@ btnC.addEventListener('click', function(){
 // const sevenBtn = document.querySelector(#seven)
 // const sevenBtn = document.querySelector(#seven)
 
-// var btnList = document.querySelectorAll(".row-button")
-//
-// function inform(press){
-//   for (let i = 0; i < btnList.length; i++) {
-//     let btn = btnList[i];
-//
-//     btn.addEventListener("click", function(event){
-//       console.log("you clicked button number " + i);
-//       console.log(btn);
-//       press = btn;
-//   })
-//
-//   }
-// }
+var btnList = document.querySelectorAll(".row-button");
+
+
+  for (let i = 0; i < btnList.length; i++) {
+    let btn = btnList[i];
+
+    btn.addEventListener("click", function(event){
+      btn = ["", 7, 8, 9, "", 4, 5, 6, "", 1, 2, 3, "", 0, "", "", ""];
+      console.log("you clicked button number " + btn[i]);
+      console.log(event);
+
+  })
+
+  }
